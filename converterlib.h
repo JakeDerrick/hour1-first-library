6e96f6976a1f10254414788ef915fab994fe18fb
JakeDerrick <31658344+JakeDerrick@users.noreply.github.com>
2017-11-02 08:10:56 -0500

    Update converterlib.h

--- converterlib.h
+++ converterlib.h
@@ -5,3 +5,10 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+float cm_to_mi(void)
+{
+    float cm = get_float("Number of centimeters:");
+    float mi = cm / 160934.4;
+    printf("\nThe number of miles is %.2f\n", mi);
+    return mi;
+}
